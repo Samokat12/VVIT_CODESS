ffb6d57242c76a6a351550be76fd83a9cf121a8f
Samokat12 <bitynarf@yandex.ru>
2025-12-01 00:56:07 +0300

    Add files via upload

---
+++ Laboratornya №3/LABA3.3.py
@@ -0,0 +1,22 @@
+path = 'Lab_3/example.txt'
+
+def func(path, method):
+    try:
+        if method == 'целиком':
+            with open(path, 'r') as file:
+                content = file.read()
+            return content
+
+        elif method == 'построчно':
+            with open(path, 'r') as file:
+                return [line.strip() for line in file]
+
+        else:
+            return 'Ошибка!'
+
+    except FileNotFoundError:
+        return('Ошибка: Файл не найден!\n' + str(e))
+        
+
+method = input('Какой формат чтения вы хотите выбрать?\nчтение целиком или построчно?: ')
+print(func(path, method))
